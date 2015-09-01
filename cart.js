9636ca930f4e845a097717b6cdb2424af5c24155
Kyle Pollich <kpollich1@gmail.com>
2015-08-31 21:32:18 -0400

    Add gitignore. Add dependency for prompt - handles input. Add functionality for user input.

--- cart.js
+++ cart.js
@@ -8,7 +8,9 @@ module.exports =
 		console.log("---------------------------------------------------");
 		console.log("Current items: ");
 		Object.keys(items).forEach(function(item) {
-			console.log("Item \"" + item + "\" has a price of $" + items[item].price + ", and there are " + items[item].quantity + " in stock.");
+			console.log("Item \"" + item + "\" has a price of $" + items[item].price + 
+				", and there are " + items[item].quantity + " in stock."
+			);
 		});
 
 		console.log("---------------------------------------------------");
@@ -18,14 +20,25 @@ module.exports =
 		console.log("\nCurrent cart in borderline illegible JSON: %j ", this.cart);
 	},
 
-	purchaseItem: function(item, quantity){
+	purchaseItem: function(item, quantity, callback){
 		if (items[item]){
-			this.cart[item] = {"quantity": quantity, "total": items[item].price * quantity};
+			if (!this.cart[item]) {
+				this.cart[item] = {"quantity": Number(quantity), "total": Number(items[item].price * quantity)};
+			}
+			else {
+				this.cart[item].quantity += Number(quantity);
+				this.cart[item].total = Number(items[item].price * this.cart[item].quantity);
+			}
 			items[item].quantity -= quantity;
-			console.log("---------------------------------------------------");
 			console.log("You successfully purchased " + quantity + " " + item + "! Here's your cart and the available items after your purchase: ");
 			this.displayCurrentCart();
 			this.displayCurrentItems();
+
+			callback();
 		}	
+	},
+
+	isItemPurchasable: function(item, quantity) {
+		return (items[item] && Number(quantity) <= Number(items[item].quantity));
 	}
 }
--- main.js
+++ main.js
@@ -1,5 +1,35 @@
 var cart = require("./cart");
+var prompt = require('prompt');
+process.stdin.setEncoding("utf-8");
 
 cart.displayCurrentItems();
-cart.purchaseItem("apples", 2);
-cart.purchaseItem("bananas", 5);
+
+var schema = {
+	properties: {
+		item: {
+			description: "Enter your choice of item"
+		},
+		quantity: {
+			description: "Enter the quantity"
+		}
+	}
+};
+
+getInput();
+
+function getInput () {
+	prompt.start();
+	prompt.get(schema, function(err, result) {
+		if (err) { return onErr(err); }
+		if (cart.isItemPurchasable(result.item, result.quantity)) {
+			cart.purchaseItem(result.item, result.quantity, getInput);
+		}
+		else {
+			console.log("Quantity too high!");
+		}
+	});
+}
+
+function onErr(err) {
+	console.log(err);
+}
